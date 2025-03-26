ef62d6a7f376e30fbe840c242fd41af8dc4c1cc3
Roshan Patel <gdevrp@gmail.com>
2025-03-26 22:57:48 +1300

    u

--- quickstart_snowflake/assets/blob_ingest.py
+++ quickstart_snowflake/assets/blob_ingest.py
@@ -1,21 +1,18 @@
 from dagster import asset
-from azure.storage.blob import BlobServiceClient
 from dagster_azure.adls2 import ADLS2Resource
+from azure.storage.blob import ContainerClient
 
 @asset(required_resource_keys={"adls2"})
 def azure_blob_file_list(context):
     # Get the adls2 resource from context
     adls2: ADLS2Resource = context.resources.adls2
     
-    # Initialize the BlobServiceClient using the storage account URL and SAS token
-    blob_service_client = BlobServiceClient(
-        account_url=f"https://{adls2.storage_account}.blob.core.windows.net",
+    # Create the ContainerClient directly using the storage account and SAS token
+    container_name = "storage"
+    container_client = ContainerClient(
+        account_url=f"https://{adls2.storage_account}.blob.core.windows.net/{container_name}",
         credential=adls2.credential.token,
     )
-    
-    # Specify the container to list files from
-    container_name = "storage"
-    container_client = blob_service_client.get_container_client(container_name)
     
     # List all blobs in the container
     file_names = []
